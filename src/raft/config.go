a5231e3a2a1319e75084e2af867d80993c65fa46
haoy <43663432+daisuki3@users.noreply.github.com>
2021-01-16 17:31:12 +0800

    Update config.go

--- src/raft/config.go
+++ src/raft/config.go
@@ -471,7 +471,6 @@ func (cfg *config) one(cmd interface{}, expectedServers int, retry bool) int {
 			time.Sleep(50 * time.Millisecond)
 		}
 	}
-	DPrintf("%v", cfg.logs[0])
 	cfg.t.Fatalf("one(%v) failed to reach agreement", cmd)
 	return -1
 }
